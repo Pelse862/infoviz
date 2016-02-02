e81de7b5ad3f987f73d5fb9eb7631e25103d5b38
pelse862 <pelle.serander@hotmail.com>
2016-02-02 15:30:10 +0100

    done

--- lab1/js/pc.js
+++ lab1/js/pc.js
@@ -113,7 +113,6 @@ function pc(){
             .text(String);
 
 
-        // Add and store a brush for each axis.
         g.append("svg:g")
             .attr("class", "brush")
             .each(function(d) { d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brush", brush)); })
@@ -126,13 +125,18 @@ function pc(){
 
     // Handles a brush event, toggling the display of foreground lines.
     function brush() {
+        var lines = [];
         var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
             extents = actives.map(function(p) { return y[p].brush.extent(); });
         foreground.style("display", function(d) {
-            return actives.every(function(p, i) {
-                return extents[i][0] <= d[p] && d[p] <= extents[i][1];
-            }) ? null : "none";
+            if(actives.every(function(p, i) { return extents[i][0] <= d[p] && d[p] <= extents[i][1]})){
+                lines.push(d["Country"]);
+                return null;
+            }
+            else
+                return "none";
         });
+        selFeature(lines);
     }
 
     // Returns the path for a given data point.
