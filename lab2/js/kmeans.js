c31dada0a954b6057673372e669506b2c7695ac5
Pelle Serander <Pelle Serander>
2016-02-07 17:12:27 +0100

    .... I'm an idiot and so is javascript

--- lab2/js/kmeans.js
+++ lab2/js/kmeans.js
@@ -7,43 +7,47 @@
    
     function kmeans(data, k) {
         
-        //...
+        //init variables
         var cycle = 0;
+        var distance = []; 
         var breakCondition = true;
 		var cycleTracker = [];
-		for(var i=0; i<k; i++) {
-			cycleTracker[i] = [];
-		}
+		var kList = [];
+		var trackList = [];
+	
+		//	Initial Step
+		//_________________________________________________________
 
-		do{
-			//	Initial Step
-			//_________________________________________________________
-			var kList = {};
-			var trackList = [];
-			for(var i=0; i<k; i++) {
-				trackList[i] = [];
-			}
-			//store index to which the line i closest
-			var closestToIndex = {};
-			
-			//get random line as initial k
-			for(var i=0; i<k; i++) {
-				kList[i] = data[Math.floor(Math.random() * data.length) + 0];
-			}
-			
+		//Get random line as initial k
+		for(var cou=0; cou<k; cou++) 
+		{
+			cycleTracker[cou] = [];
+			trackList[cou] = [];
+			var random =  data[ Math.floor(Math.random()* data.length)];
+			kList.push( [ parseFloat(random.A), parseFloat(random.B), parseFloat(random.C)] );
+
+		}
+	
+		console.log(kList);
+		//store index to which the line i closest
+		var closestToIndex = {};
+		
+
+		do
+		{
 			
 			var count = 0;
 			//do for each line
-	        data.forEach(function(d){
+	        data.forEach(function(d){ 
 				
-				var distance = {}; 
+				distance = {}; 
 				var smallestDistIndex = 0;
 				
 				//calculate distance to kCenter point
 				for(var i=0; i<k; i++){
-					distance[i] =  Math.sqrt( Math.pow(( kList[i].A - d.A ) , 2) +
-														Math.pow(( kList[i].B - d.B ) , 2) +
-														Math.pow(( kList[i].C - d.C ) , 2) );
+					distance[i] =  Math.sqrt( Math.pow(( kList[i][0] - d.A ) , 2) +
+														Math.pow(( kList[i][1]  - d.B ) , 2) +
+														Math.pow(( kList[i][2]  - d.C ) , 2) );
 					if(distance[i] < distance[smallestDistIndex])
 						smallestDistIndex = i;
 				}
@@ -52,10 +56,12 @@
 				
 				trackList[smallestDistIndex][count] = d;
 				
-				
 				count++;
 			})
-		
+
+			
+
+
 			// Step 2
 			//_________________________________________________________
 		
@@ -67,58 +73,69 @@
 				//calculate new K centroids for each cluster
 				trackList[i].forEach(function(d){
 					sumDistA += parseFloat(d.A);
+
 					sumDistB += parseFloat(d.B);
 					sumDistC +=  parseFloat(d.C);
 					numberOfElements += 1;
 
 				})
-				
 
-				kList[i].A = sumDistA/trackList[i].length;
-				kList[i].B = sumDistB/trackList[i].length;
-				kList[i].C = sumDistC/trackList[i].length;
+				//NaN failsafe
+				if(numberOfElements == 0){
+					numberOfElements=1;
+				
+				}
+
+				kList[i][0] = sumDistA/numberOfElements;
+				kList[i][1]= sumDistB/numberOfElements;
+				kList[i][2] = sumDistC/numberOfElements;
 				
 			}
+
 
 
 			//Step3
 			//________________________________________________________
 			//check qualit of cluster using sum of square distances
 			
-		
 			for(var i=0; i<k; i++) {
 				var sumOfSquareA = 0;
 				var sumOfSquareB = 0;
 				var sumOfSquareC = 0;
 		
 				trackList[i].forEach(function(d){
-					sumOfSquareA += Math.pow( parseFloat(d.A) - kList[i].A ,2)
-				
-					sumOfSquareB += Math.pow( parseFloat(d.B) - kList[i].B ,2)
-					sumOfSquareC += Math.pow( parseFloat(d.C) - kList[i].C ,2)
+					sumOfSquareA += Math.pow( parseFloat(d.A) - kList[i][0] ,2)
+					sumOfSquareB += Math.pow( parseFloat(d.B) - kList[i][1] ,2)
+					sumOfSquareC += Math.pow( parseFloat(d.C) - kList[i][2] ,2)
 				})
+
 				cycleTracker[i][cycle] = [sumOfSquareA,sumOfSquareB,sumOfSquareC];
 
 			}
-	
-			if(cycle != 0 ){
+			
+			if(cycle != 0 )
+			{
 				var dist = {};
 				for(var i=0; i<k; i++) {
-					dist[i]= (cycleTracker[i][cycle-1][0] - cycleTracker[i][cycle][0])+
+
+					dist[i]= Math.abs(cycleTracker[i][cycle-1][0] - cycleTracker[i][cycle][0])+
 							 (cycleTracker[i][cycle-1][1] - cycleTracker[i][cycle][1])+  
-							 ( cycleTracker[i][cycle-1][2] - cycleTracker[i][cycle][2]);
+							 (cycleTracker[i][cycle-1][2] - cycleTracker[i][cycle][2]);
 							 
 				}
 				
+				//Step 4 Validate distance?
+				//_______
 				var c = 0;
-				console.log(dist);
+			
 				for(var i=0; i<k; i++) {
-					if(dist[i] < k){
+					if(isNaN(dist[i]) || dist[i] < 0)dist[i] = 0;
+					if(dist[i] < 0.01){
 						c++;
 					}
 				}
 				
-		
+				//if no one has a differance greater than 0.01 it's ok
 				if( c == k){
 					breakCondition = false;
 				}
@@ -127,10 +144,16 @@
 				}
 				
 			}
-			console.log(cycle);
+			
 			cycle++;
 			
 
-		}while(breakCondition)
+		}
+		while(breakCondition)
+		console.log(cycleTracker)
 
+
+		return closestToIndex;
+		
+		
     };
